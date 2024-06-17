d647ca0e0d31fe90a7e1a799677cf447adee19a9
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-17 09:34:05 +0300

    Added the flexbox

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,19 +1,39 @@
-import {Image, StyleSheet, Platform, View, Text} from 'react-native';
-
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 
 export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello world</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Add the course goal" />
+        <Button title="Add the goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 5,
+    marginBottom: 24,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  goalsContainer: {
+    flex: 3,
+  },
 });
